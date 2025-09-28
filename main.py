b2ce15a029f2c6c4d50bad1c34b051a668bbd1c4
insgesichtnicht <kedavran@gmail.com>
2025-09-28 10:48:40 +1000

    made all objects, up to collision

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import pygame
+from asteroidfield import AsteroidField
 from constants import *
+from player import Player
+from asteroid import Asteroid
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
+    ship = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    field = AsteroidField()
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        
+        updatable.update(dt)
         screen.fill("black")
+        
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
